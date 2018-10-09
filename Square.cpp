2fd7c96ae652673e87489b500c519bfa9e171f9e
legends-assemble <43819280+legends-assemble@users.noreply.github.com>
2018-10-09 12:49:51 +0000

    Update Square.cpp

--- Square.cpp
+++ Square.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int main(){
-   int num;//getti
+   int num;//initialising integer
+   cout<<"please enter aan integer";
+   cin>>num;
+   if(num<0){
+      num*=-1;
+      }
+   int SumOfInteger;
    }
